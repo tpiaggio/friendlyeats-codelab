d947baca344c09df75bcdd80602f1615d846a130
Tomas Piaggio <tomas@createthrive.com>
2023-12-28 12:11:01 -0300

    first commit

---
+++ src/lib/firebase/config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyDzV6xcQ0VTlq61Y6PfF4hLdG0TNdGp3YE",
+  authDomain: "friendlyeats-codelab-ca68f.firebaseapp.com",
+  projectId: "friendlyeats-codelab-ca68f",
+  storageBucket: "friendlyeats-codelab-ca68f.appspot.com",
+  messagingSenderId: "928431631163",
+  appId: "1:928431631163:web:abb7c084cc88177ae8251e",
+};
